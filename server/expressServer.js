f189b854e7a349c84795dcdbe661bb67c9d4d503
Michael Albinson <mikealbinson@gmail.com>
2016-11-22 18:20:09 -0500

    Moving everything client into the client folder
    PULL BEFORE YOU KEEP WORKING

--- server/expressServer.js
+++ server/expressServer.js
@@ -9,31 +9,31 @@ const PORT = 8080;
 var server = express();
 
 // directories from which we serve css, js and assets statically
-server.use('/css', express.static('../css'));
-server.use('/js', express.static('../js'));
-server.use('/assets', express.static('../assets'));
+server.use('/css', express.static('../client/css'));
+server.use('/js', express.static('../client/js'));
+server.use('/assets', express.static('../client/assets'));
 
 server.use(cp()); // imports all the cookie-parser middleware to express
 
 server.get('/', function(request, response) { // default link, delivers landing page
 	// validateSession(); // this needs to be at the beginning of every request
-	response.sendFile(path.join(__dirname, '..', 'html/index.html'));
+	response.sendFile(path.join(__dirname, '..', 'client/html/index.html'));
 	response.cookie("the first cookie", "the value of my cookie")
 });
 
 server.get('/search', function(request, response) { // to search bar
 	// validateSession(); // this needs to be at the beginning of every request
-	response.sendFile(path.join(__dirname, '..', 'html/index.html'));
+	response.sendFile(path.join(__dirname, '..', 'client/html/index.html'));
 });
 
 server.get('/question/id=\*', function(request, response) { // question page, queried by id
 	// validateSession(); // this needs to be at the beginning of every request
 	var pertinentQuery = request.url.replace('/question/id=', '');
-	response.sendFile(path.join(__dirname, '..', 'html/Q_APage.HTML'));
+	response.sendFile(path.join(__dirname, '..', 'client/html/Q_APage.HTML'));
 });
 
 server.get('/about', function(request, response) { //about page
-	response.sendFile(path.join(__dirname, '..', 'html/about.HTML'));
+	response.sendFile(path.join(__dirname, '..', 'client/html/about.HTML'));
 })
 
 server.get('/new', function(request, response) { // newest questions being asked in list view
