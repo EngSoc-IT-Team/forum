0be6e9ed2dab81dcb302b2381985d7c1c99e8a9e
Michael Albinson <mikealbinson@gmail.com>
2016-12-02 01:29:39 -0500

    First go round at creating mock data
    Changes to config file, default tables, etc. to facilitate data addition

--- server/setup.js
+++ server/setup.js
@@ -7,26 +7,18 @@ var log = require('./util/log');
 
 var fs = require('fs');
 var path = require('path');
-var possibleTypes = JSON.parse(fs.readFileSync(path.join(__dirname, 'config/config.json'), 'utf8'));
-var createDemoData = possibleTypes["createDemoData"];
+var preferences = JSON.parse(fs.readFileSync(path.join('./config/config.json'), 'utf8'));
 
-dependencies.checkDependencies();
-dbsetup.setupDatabase();
-setTimeout(createSampleUser, 1000);
 
-// this will be replaced by a full set of sample data
-function createSampleUser() {
-	if (createDemoData) {
-		log.info("Creating demo data")
-		var user = new dbr.DBRow('user');
-		user.setValue("netid", "anon");
-		user.setValue("id", "1234567890abcdefghijklmnopqrstuv");
-		user.setValue("username", "anon");
-		user.setValue("dateJoined", "2016-11-23");
-		user.insert().then(function(res) {
-			log.info("Sample user 'anon' sucessfully created")
-		}, function(res) {
-			log.error("There was an error inserting the sample data into the database, see above errors");
-		})
-	}
+setup(); // let's set everything up
+
+
+function setup() {
+	dependencies.checkDependencies();
+
+	if (preferences["databaseSetupNeeded"])
+		dbsetup.setupDatabase(); // setup all default tables
+
+	if (preferences["loadMockData"])
+		setTimeout(function() {dbsetup.loadDemoData()}, 1000); //wait for database to get set up before we try to insert mock data
 }
--- server/util/setup/SetupCoreDatabase.js
+++ server/util/setup/SetupCoreDatabase.js
@@ -3,13 +3,62 @@
 var fs = require('fs');
 var path = require('path')
 var db = require('../DatabaseManager.js');
+var dbr = require('../DBRow.js');
 var log = require('../log.js')
 
 var possibleTypes = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'config/SQLDatatypes.json'), 'utf8')) 
 var defaults = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'config/defaultTables.json'), 'utf8')) 
+var demodata = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'config/mockData.json'), 'utf8')) 
 
 var dbm = new db.DatabaseManager();
 var numCreated = 0;
+
+exports.setupDatabase = function() { 
+	log.info("~~~~~~~~~~~~~~Starting Database Setup~~~~~~~~~~~~~~~")
+
+	var totalLength = Object.keys(defaults).length;
+	numCreated = 0;
+
+	for (var prop in defaults) {
+		createTable(defaults[prop]["tablename"], defaults[prop]["fields"], totalLength, numCreated);
+	}
+}
+
+exports.loadDemoData = function() {
+	log.info("~~~~~~~~~~~~~~Loading Demo Data~~~~~~~~~~~~~~~")
+	var totalLength = Object.keys(demodata).length;
+	numCreated = 0;
+	for (var element in demodata) {
+		loadRow(demodata[element]["table"], demodata[element], totalLength, numCreated);
+	}
+}
+
+function loadRow(table, fields, numElementsToCreate, numberCreated) {
+	var newRow = new dbr.DBRow(table);
+	for (var field in fields) {
+		if (field == "table")
+			continue
+
+		newRow.addQuery(field, fields[field]);
+	}
+
+	var id = newRow.getValue("id");
+
+	newRow.insert().then(function() {
+		log.info("Example row no. " + numCreated + " created!");
+		numCreated++;
+		if (numElementsToCreate == numCreated)
+			log.info("~~~~~~~~~~~~~~~End of Demo Data Load~~~~~~~~~~~~~~~~\n\n");
+	}, function(err){
+		log.warn("Row no. " + numCreated + " not inserted, likely due to a previously logged error");
+		log.warn("The id of the failing row was '" + id + "'");
+		numCreated++;
+		if (numElementsToCreate == numCreated)
+			log.info("~~~~~~~~~~~~~~~End of Demo Data Load~~~~~~~~~~~~~~~~\n")
+
+	});
+}
+
 
 function createTable(tableName, fields, numberTablesToCreate, numberCreated) { //TODO: make this more intelligent to match changes to defaultTables.json
 	var keyCount = 0;
@@ -18,8 +67,14 @@ function createTable(tableName, fields, numberTablesToCreate, numberCreated) { /
 	for (var field in fields) {
 		if(keyCount < Object.keys(fields).length-1) {
 			queryString += field + ' ' + possibleTypes[fields[field]["type"]];
-			if (fields[field]["default"])
-				queryString += " DEFAULT '" + fields[field]["default"] + "'";
+			if (fields[field]["default"]) { 
+				if (fields[field]["default"] == 'CURRENT_TIMESTAMP' || fields[field]["default"] == 'GETDATE()') {
+					queryString += " DEFAULT " + fields[field]["default"];
+				}
+				else {
+					queryString += " DEFAULT '" + fields[field]["default"] + "'";
+				}
+			}
 
 			if (fields[field]["primaryKey"])
 				queryString += " PRIMARY KEY"
@@ -32,8 +87,14 @@ function createTable(tableName, fields, numberTablesToCreate, numberCreated) { /
 		}
 		else {
 			queryString += field + ' ' + possibleTypes[fields[field]["type"]];
-			if (fields[field]["default"])
-				queryString += " DEFAULT '" + fields[field]["default"] + "'";
+			if (fields[field]["default"]){ //make this better
+				if (fields[field]["default"] == 'CURRENT_TIMESTAMP' || fields[field]["default"] == 'GETDATE()') {
+					queryString += " DEFAULT " + fields[field]["default"];
+				}
+				else {
+					queryString += " DEFAULT '" + fields[field]["default"] + "'";
+				}
+			}
 
 			if (fields[field]["primaryKey"])
 				queryString += " PRIMARY KEY"
@@ -54,17 +115,4 @@ function createTable(tableName, fields, numberTablesToCreate, numberCreated) { /
 			log.info("~~~~~~~~~~~~~~~End of Database Setup~~~~~~~~~~~~~~~~\n")
 
 	});
-}
-
-exports.setupDatabase = function() { 
-	log.info("~~~~~~~~~~~~~~Starting Database Setup~~~~~~~~~~~~~~~")
-
-	var totalLength = Object.keys(defaults).length;
-	numCreated = 0;
-
-	for (var prop in defaults) {
-		createTable(defaults[prop]["tablename"], defaults[prop]["fields"], totalLength, numCreated);
-	}
-	
-	
 }
