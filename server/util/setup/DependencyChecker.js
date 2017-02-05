f0239ec12ad0d07477423ea9fc51f40cf9e22652
Michael Albinson <mikealbinson@gmail.com>
2017-02-04 21:28:07 -0500

    Adding subscription emailing dependencies
    Note that nodemailer and nodemailer-smtp-transport must now be npm
    installed to start the server

--- server/util/setup/DependencyChecker.js
+++ server/util/setup/DependencyChecker.js
@@ -2,37 +2,40 @@
 
 var log = require('../log');
 
-const dependencies = ['mysql', 'express', 'path', 'fs', 'cookie-parser', 'body-parser']; //'client-session' might need to be added
+const dependencies = ['mysql', 'express', 'path', 'fs', 'cookie-parser', 'body-parser', 'nodemailer', 'nodemailer-smtp-transport'];
 
 exports.checkDependencies = function() {
 	log.info("~~~~~~~~~~~~~~~~Module Check~~~~~~~~~~~~~~~~~");
 	var modulesYouNeed = [];
 	for (var i in dependencies) {
-		try {
-			var testrequire = require(dependencies[i])
-			log.info("Cool, looks like the module " + dependencies[i] + " is already installed!")
-		} catch (e) {
-			log.warn("Uh oh, looks like you're missing the module " + dependencies[i]);
-			modulesYouNeed.push(dependencies[i]);
-		} 
+		if (dependencies.hasOwnProperty(i)) {
+            try {
+                require(dependencies[i]);
+                log.info("Cool, looks like the module " + dependencies[i] + " is already installed!")
+            } catch (e) {
+                log.warn("Uh oh, looks like you're missing the module " + dependencies[i]);
+                modulesYouNeed.push(dependencies[i]);
+            }
+        }
 	}
 	
 	logSummary(dependencies, modulesYouNeed);
 	log.info("~~~~~~~~~~~~~~~End Module Check~~~~~~~~~~~~~~~~\n\n");
-}
+};
 
 function logSummary(dependencyList, modulesNotInstalled) {
 	log.info("~~~~~~~~~~~~~~~~~~~~Summary~~~~~~~~~~~~~~~~~~~");
-	log.info((dependencyList.length - modulesNotInstalled.length) + "/" +dependencyList.length + " modules installed")
+	log.info((dependencyList.length - modulesNotInstalled.length) + "/" +dependencyList.length + " modules installed");
 
 	if (modulesNotInstalled.length == 0) {
-		log.info("You have all the modules necesary to run this service!")
+		log.info("You have all the modules necesary to run this service!");
 		return true;
 	}
 
 	log.warn("You still need the module(s):\n");
-	for(var mods in modulesNotInstalled) { 
-		log.warn(modulesNotInstalled[mods] + "\n");
+	for(var mods in modulesNotInstalled) {
+		if (modulesNotInstalled.hasOwnProperty(mods))
+			log.warn(modulesNotInstalled[mods] + "\n");
 	}
 
 	throw new Error("You can't run this service until all the modules are installed, check the previous module logs\n");
