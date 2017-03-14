d04b2b9bd01102cf581e86e63c25f080fde46fd2
Ayrtonfoster <ayrtonfoster@gmail.com>
2017-03-14 00:43:41 -0400

    Idea for new comment box on question and answer page
    Let me know what you think of this instead of the modal, still messing
    around. Thinking of not even having a button and just having the comment
    box there. Anywway let me know what you guys think

--- client/js/question.js
+++ client/js/question.js
@@ -20,6 +20,7 @@ var questionTemplate = '<div class="info-block row">\
                                 <p class="description">{7}</p>\
                                 <div class="clearfix">\
                                     <button type="button" class="btn btn-sm button" data-toggle="modal" data-target="#myModal">Comment</button>\
+                                    <button type="button" class="btn btn-sm button" data-toggle="collapse" data-target="#demo">Collapsible</button>\
                                     {8}\
                                 </div>\
                             </div>\
