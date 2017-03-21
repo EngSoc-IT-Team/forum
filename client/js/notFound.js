5837c9fcdc8980d0e763a2190cb0bd089b9df4a1
Michael Albinson <mikealbinson@gmail.com>
2017-03-20 23:52:36 -0400

    FIX: inline javascript
    Man I was lazy

---
+++ client/js/notFound.js
@@ -0,0 +1,9 @@
+/**
+ * notFound.js
+ * Written by Michael Albinson 3/20/17
+ */
+
+"use strict";
+
+function addUrl() {
+    $('#urlText').html('The URL: <b><a class="body-link" href="' + window.location.href + '">' + window.location.href + '</a></b> could not be found.');}
