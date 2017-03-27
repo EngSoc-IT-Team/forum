7399e86a280a31762fc69286c94263083df9f880
Michael Albinson <mikealbinson@gmail.com>
2017-03-27 03:42:05 -0400

    Reporter Documentation

--- server/util/actions/Reporter.js
+++ server/util/actions/Reporter.js
@@ -12,19 +12,47 @@ var DBRow = require('../DBRow').DBRow;
 var lit = require('../Literals');
 var log = require('../log');
 
-/*
+/**
  * Enters the report into the database
  *
+ * @param userId
+ * @param itemId
+ * @param reportReason
+ * @param reportContent
  */
-
 exports.sendReport = function(userId, itemId, reportReason, reportContent) {
     return new Promise(function(resolve, reject) {
+        exports.hasBeenReported(userId, itemId).then(function(res) {
+            if (res)
+                reject('sendReport error: This item has already been reported by this user');
+
+            var report = new DBRow(lit.REPORT_TABLE);
+            report.setValue(lit.FIELD_USER_ID, userId);
+            report.setValue(lit.FIELD_RELATED_ITEM_ID, itemId);
+            report.setValue(lit.FIELD_REPORT_REASON, reportReason);
+            report.setValue(lit.FIELD_REPORT, reportContent);
+            report.insert().then(function() {
+                resolve(true);
+            }).catch(function (err) {
+                log.error("sendReport error: " + err);
+                reject(false);
+            });
+        });
+
+    });
+};
+
+/*
+ * Resolves the report in the database so that it is known that the issue in the report
+ * has been fixed
+ *
+ * @param reportId The id of the report to resolve.
+ */
+exports.resolveReport = function(reportId) {
+    return new Promise(function(resolve, reject) {
         var report = new DBRow(lit.REPORT_TABLE);
-        report.setValue(lit.FIELD_USER_ID, userId);
-        report.setValue(lit.FIELD_RELATED_ITEM_ID, itemId);
-        report.setValue(lit.FIELD_REPORT_REASON, reportReason);
-        report.setValue(lit.FIELD_REPORT, reportContent);
-        report.insert().then(function() {
+        report.getRow(reportId).then(function() {
+            // TODO: resolve the report somehow
             resolve(true);
         }).catch(function (err) {
             log.error("sendReport error: " + err);
@@ -33,17 +61,21 @@ exports.sendReport = function(userId, itemId, reportReason, reportContent) {
     });
 };
 
-/*
- * Resolves the report in the database so that it is known that the issue in the report
- * has been fixed
- * @param reportId The id of hte report to resolve.
+/** Checks to see if the given user has already reported the provided item
+ *
+ * @param userID the ID of the user who reported the item
+ * @param itemID the ID of the reported item
  */
-exports.resolveReport = function(reportId) {
+exports.hasBeenReported = function(userID, itemID) {
     return new Promise(function(resolve, reject) {
         var report = new DBRow(lit.REPORT_TABLE);
-        report.getRow(reportId).then(function() {
-            // TODO: resolve the report somehow
-            resolve(true);
+        report.addQuery(lit.FIELD_USER_ID, userID);
+        report.addQuery(lit.FIELD_ITEM_ID, itemID);
+        report.query(reportId).then(function() {
+            if (report.next())
+                resolve(report);
+            else
+                resolve(false);
         }).catch(function (err) {
             log.error("sendReport error: " + err);
             reject(false);
