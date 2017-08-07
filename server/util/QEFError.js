93108cb4c9dfce0c0ea05d475f3b9332bda34c4f
Michael Albinson <mikealbinson@gmail.com>
2017-08-06 23:22:39 -0400

    QEFError, wrapping errors and allowing custom errors
    A new proprietary error that we can throw instead of more generic error
    types, we can also extend this class to create new, even more specific
    errors

---
+++ server/util/QEFError.js
@@ -0,0 +1,47 @@
+/**
+ * Written by Michael Albinson
+ * 04/08/17
+ */
+
+"use strict";
+
+/**
+ * A class designed to create user understandable error messaging that does not expose stack traces to the client on
+ * error. If the error is internal, we allow the stack to be applied to the object, if it is external, we suppress the
+ * stack and just pass the error message.
+ *
+ * @constructor
+ */
+
+var log = require('./log.js');
+
+module.exports = function(err) {
+    this.name = "QEFError";
+    this.message = "An internal server error occurred"; // Default error message
+    this.stack = undefined;
+
+    var isInternal = false;
+
+    if (!err.message && !err.stack) { // if the error has no stack or message property, it is created by our code
+        this.message = err;
+        isInternal = true;
+    }
+
+    if (!isInternal) {
+        if (err.message) //if we get a message, sterilize it
+            this.message = _sterilize(err.message);
+
+        if (err.stack && isInProduction) //if we get a stack, check if we're in production and safely pass the stack if we are, otherwise keep it to ourselves
+            return this;
+    }
+
+    function _sterilize(msg) {
+        return msg;
+    }
+
+    function _stack(isInProduction) {
+
+    }
+
+    log.error("** SYSTEM SEVERE ** " + this.message);
+};
