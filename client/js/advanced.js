cc2bedef30104a3ef82cf1bb440f880eb5d7aabb
Ayrtonfoster <ayrtonfoster@gmail.com>
2017-10-26 23:33:24 -0400

    advanced page query string works
    works for most part, let me know of someting doesnt work

--- client/js/advanced.js
+++ client/js/advanced.js
@@ -11,7 +11,38 @@ var select2Options = {data: [], tags: true, tokenSeparators: [',', ' '], width: 
 function sendQuery() {
     //get params as /list?advanced=true&table=<TABLE>&tags=<TAGS>&keywords=<KEYWORDS COMMA SEPARATED NO SPACES>&exactPhrase=<EXACT PHRASE>&titleContains=<TITLE WORDS COMMA SEPARATED>
     // if a text field is empty DO NOT INCLUDE IT in the query
-    location.href = '/list?advanced=true&'; //add params here, it will reload the page
+
+    //Use jQuery to get the innerText of all the fields
+    //function getLoginContent() {
+
+        var tags = $('#tags')[0].value;
+        var keywords = $('#keywords')[0].value;
+        var exactWords = $('#exact-words')[0].value;
+        var title = $('#title')[0].value;
+    //}
+    //Initialize the queryString variable with the selected table to search
+    var queryString  = '/list?advanced=true&table='+currentButton.replace('#','');
+    //if (fieldText) // <- ie: if the field has text in it
+        //queryString += fieldName + "=" + fieldText
+    if(tags.length > 0){
+        queryString += '&tags='+tags.replace(' ', '');
+    }
+
+    if(keywords.length > 0){
+        queryString += '&keywords='+keywords.split(" ");
+    }
+
+    if(exactWords.length > 0){
+        queryString += '&exactPhrase='+exactWords.replace(' ', '');
+    }
+
+    if(title.length > 0) {
+        //var res = title.split(" ");
+        queryString += '&titleContains='+title.split(" ");
+    }
+    //repeat for all fields
+
+    location.href = queryString //add params here, it will reload the page
 }
 
 var currentButton = 'all'; // hint, this is the table name
