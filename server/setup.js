9c9d1639d0ed52842123709510bc591b9a86191e
Michael Albinson <mikealbinson@gmail.com>
2017-11-10 00:10:16 -0500

    Cleanup of setup
    Should be a teensy bit cleaner and more efficient now

--- server/setup.js
+++ server/setup.js
@@ -11,9 +11,6 @@ var dependencies = require('./util/setup/DependencyChecker.js');
 var dbsetup = require('./util/setup/SetupCoreDatabase.js');
 var log = require('./util/log');
 var lit = require('./util/Literals.js');
-
-var fs = require('fs');
-var path = require('path');
 var PM = require('./util/PropertyManager');
 
 
@@ -25,22 +22,21 @@ setup(); // let's set everything up
  */
 function setup() {
 	dependencies.checkDependencies();
+    if (PM.getConfigProperty(lit.DATABASE_SETUP_NEEDED)) {
+        dbsetup.checkIfDataBaseExistsAndCreateIfNecessary().then(function () {
+            return dbsetup.setupDatabase().catch(function() {
+                log.error('THERE WAS AN ERROR DURING DATABASE SETUP');
+                log.error('PLEASE SEE THE ERROR LOGS FOR MORE INFORMATION');
+			}); // setup all default tables
 
-    dbsetup.checkIfDataBaseExistsAndCreateIfNecessary().then(function() {
-		if (PM.getConfigProperty(lit.DATABASE_SETUP_NEEDED))
-        	return dbsetup.setupDatabase(); // setup all default tables
+        }).then(function () {
+            // only load demo data if the database schema has just been set up
+            if (PM.getConfigProperty(lit.LOAD_MOCK_DATA) && PM.getConfigProperty(lit.DATABASE_SETUP_NEEDED))
+                return dbsetup.loadDemoData();
 
-	}).then(function() {
-		// only load demo data if the database schema has just been set up
-		if (PM.getConfigProperty(lit.LOAD_MOCK_DATA) && PM.getConfigProperty(lit.DATABASE_SETUP_NEEDED))
-			return dbsetup.loadDemoData();
-
-	}).catch(function() {
-		log.error('THERE WAS AN ERROR DURING DATABASE SETUP');
-		log.error('PLEASE SEE THE ERROR LOGS FOR MORE INFORMATION');
-	});
+        }).catch(function () {
+            log.error('THERE WAS AN ERROR DURING DATABASE SETUP');
+            log.error('PLEASE SEE THE ERROR LOGS FOR MORE INFORMATION');
+        });
+    }
 }
-
-process.on('unhandledRejection', function(e) {
-	log.error(e);
-});
